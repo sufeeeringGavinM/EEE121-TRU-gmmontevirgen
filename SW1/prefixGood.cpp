587085df172a0a5958fb7cc55b1963833d168b2b
nitra <nitra@nitras-Air.lan>
2024-03-30 15:31:40 +0800

    updated prefixGood

--- SW1/prefixGood.cpp
+++ SW1/prefixGood.cpp
@@ -7,20 +7,18 @@ using namespace std;
 
 class TrieNode{
     public:
-        char data; //either a one or a zero / dit or dah (foreshadowing)
-        TrieNode *children[2]; //binary !! max of two nodes only per node UPDATE: I REGRET THIS HOW WOULD I HANDLE DUPLICATES!? UPDATE2: ok just do add anothe property of the node indicating how many copies of it exist (if ther are)
+        // char data; //either a one or a zero / dit or dah (foreshadowing)
+        TrieNode* children[2]; //binary !! max of two nodes only per node UPDATE: I REGRET THIS HOW WOULD I HANDLE DUPLICATES!? UPDATE2: ok just do add anothe property of the node indicating how many copies of it exist (if ther are)
         bool is_leaf; 
         int copies; //number of copies
-        TrieNode(char ooz); //oneorzero (mr robot reference)
+        TrieNode(){ //oneorzero (mr robot reference)
+            // data=ooz; //realized later on i didnt really need this
+            is_leaf=false;
+            copies=0; //by default, no copies
+            children[0]=NULL;
+            children[1]=NULL;
+        }
 };
-
-TrieNode::TrieNode(char ooz){
-    data=ooz;
-    is_leaf=false;
-    int copies=0; //by default, no copies
-    children[0]=NULL;
-    children[1]=NULL;
-}
 
 class Trieee{
     public:
@@ -32,15 +30,14 @@ class Trieee{
         string biggest; //track biggest string for special case where no common prefixes exist
         TrieNode* biggestNode;
         Trieee(){
-            root=new TrieNode('\0');
+            root=new TrieNode();
         }
         void insert(string key){
             TrieNode* toCrawl=root;
-           
             for(int i=0; i<key.size(); i++){
                 int index=key[i]-'0';
                 if((toCrawl->children[index])==NULL){
-                    toCrawl->children[index]=new TrieNode(key[i]);
+                    toCrawl->children[index]=new TrieNode(); // children[0] having a new TrieNode means a zero, children[1] means a one. traversing through the trie node for a string 101 would then be like root->children[1]->children[0]->children[1].
                     numberOfNodes++; //tracker of num of nodes
                 }
                 if(toCrawl->children[index]->is_leaf==true){
@@ -52,80 +49,84 @@ class Trieee{
                 numberOfDigits++; //tracker of num of digits. we're going through key.size() might as well add a O(1) thingy here to collect key.size()'s value
             }
             toCrawl->is_leaf=true; 
+            
             if(key.size()>biggest.size()){
                 biggest=key;
-                biggestNode=toCrawl; //note that biggest node will never have a duplicate as if it does, this function would have returned already!
+                biggestNode=toCrawl; //note that biggest node will never have a copy as if it does, this function would have returned already!
             }
         }
         void findPrefixes(TrieNode* node, string pref) //obtains all common prefixes, puts them in a vector of strings
         {   
             if(node == NULL)
                 return;
-            
-            if(node->children[0] != NULL && node->children[1] !=NULL){ // a prefix is something with one or two children
+            if(node->children[0] != NULL && node->children[1] !=NULL){ // a prefix is something with at least two children
                 twoprefixes.push_back(pref); // collect prefix itself
                 prefixNodes.push_back(node); // collect node 
             }
             if(node->children[0] != NULL){
-                findPrefixes(node->children[0], pref+'0'); //goes through entire trie, similar to 
+                findPrefixes(node->children[0], pref+'0'); // adds a 0 to pref, as children[0] being not null indicates a zero
             }
 
             if(node->children[1] != NULL){
-                findPrefixes(node->children[1], pref+'1');
+                findPrefixes(node->children[1], pref+'1'); // adds a 1 instead, as children[1]  beign not null means a one
             }
             //for the recursion to go thru the entire tree , I followed how this article below did it. though instead of with the aim of printing everything, it was just to find those with at least two children (prefixes) 
             // https://www.ritambhara.in/print-all-words-in-a-trie-data-structure/
         }
-        void getNumberofChildren(TrieNode* node, int &children){
+        void getNumberofChildren(TrieNode* node, int &substrings){
             if(node==NULL){
                 return;
             }
             if(node->is_leaf){
-                children++;
-                children+=node->copies; // if a leaf node has copies, add them
+                substrings++;
+                substrings+=node->copies; // if a leaf node has copies, add them
             }
             if(node->children[0]!=NULL){
-                getNumberofChildren(node->children[0], children);
+                getNumberofChildren(node->children[0], substrings);
                 
             }
             if(node->children[1]!=NULL){
-                getNumberofChildren(node->children[1], children);
+                getNumberofChildren(node->children[1], substrings);
             }
         }
         void printDetails(){
 
             string longestPrefix;
             int longestPG=0;
-            int children=0;
-            int mostChildren=0;
+            int substrings=0;
+            int mostsubstrings=0;
             int currentPrefixSize;
             int currentPG;
             int longestPrefixSize=0;
             findPrefixes(root, "");
             
             for(int i=0; i<twoprefixes.size(); i++){
-                children=0;
-                getNumberofChildren(prefixNodes[i], children);
+                substrings=0;
+                getNumberofChildren(prefixNodes[i], substrings);
                 currentPrefixSize=twoprefixes[i].size();
-                currentPG=currentPrefixSize*children;
+                currentPG=currentPrefixSize*substrings;
                 if(currentPrefixSize>longestPrefixSize && currentPG >= longestPG){
                     longestPG=currentPG;
-                    mostChildren=children;
+                    mostsubstrings=substrings;
                     longestPrefix=twoprefixes[i];
                     longestPrefixSize=currentPrefixSize;
                 }
             }
-            if(mostChildren==0 || biggest.size()>longestPG){ //special case when there are no common prefixes
+            if(mostsubstrings==0 || biggest.size()>longestPG){ //special case when there are no common prefixes
                 //the largest string will simply result in the greatest prefix goodness, as max prefix length in this case is just the entire string itself, and no. of substrings is 1
-                mostChildren=1;
+                mostsubstrings=1;
                 longestPrefixSize=biggest.size();
                 longestPG=longestPrefixSize;
             }
             cout<<"Number of digits in all binary strings: " << numberOfDigits <<endl;
             cout<<"Number of nodes in Trie: "<< numberOfNodes << endl;
             cout<<"Max Prefix Length : " << longestPrefixSize << endl;
-            cout<<"Total Substrings : " << mostChildren << endl;
+            cout<<"Total Substrings : " << mostsubstrings << endl;
             cout<<"Prefix Goodness : " << longestPG << endl;
+            
+
+            // DEBUGGING PURPOSES 
+            // display each possible prefix
             
             // for(int i=0; i<twoprefixes.size(); i++){
             //     cout << twoprefixes[i] << endl;
