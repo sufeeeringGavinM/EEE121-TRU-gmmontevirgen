9e24ca93354b8429c055173b016a2016ba8d6ea1
nitra <nitra@nitras-MacBook-Air.local>
2024-04-30 17:35:15 +0800

    updated grapphh.cpp

--- week8/ggraph.cpp
+++ week8/ggraph.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
-
-//just use vector of Stuff
-
-#include <iostream>
-#include <vector>
-#include <map>
 #include <string>
-#include <ctime>
-#include <chrono>
-#include <cmath>
-#include <iomanip>
 using namespace std;
 
 vector<string> parser(string splitMe){ // taken from https://www.scaler.com/topics/split-string-in-cpp/ 
@@ -42,43 +30,177 @@ vector<string> inputter(){
     return parsed;
 }
 
+class Node {
+    public:
+        string data;
+        vector<Node*> to;
+        vector<Node*> from;
+        int Tweight; // weight it takes to GO to this node
+        Node(string data, Node* to = NULL, Node* from = NULL);
+};  
 
-void inputDetect(vector<string> command, vector<vector<int>> graph){
+Node::Node(string data, Node* to, Node* from):data(data),to(to),from(from) {}
+
+
+int inputDetect(vector<string> command, vector<Node*> graph){
     if(command[0]=="DINIT"){
-     
+        if(command.size()==2){
+            return 1;
+        }
+        else{
+            return 0;
+        }
     }
     if(command[0]=="UINIT"){
-
+        if(command.size()==2){
+            return 2;
+        }
+        else{
+            return 0;
+        }
     }
-     if(command[0]=="VADD"){
-        
+    if(command[0]=="VADD"){
+        if(command.size()==2){
+            return 3;
+        }
+        else{
+            return 0;
+        }
     }
      if(command[0]=="EADD"){
-        
+        if(command.size()==3){
+            return 4;
+        }
+        else{
+            return 0;
+        }
     }
-     if(command[0]=="VDEL"){
-        
+    if(command[0]=="VDEL"){
+        if(command.size()==2){
+            return 5;
+        }
+        else{
+            return 0;
+        }
     }
      if(command[0]=="EDEL"){
-        
+        if(command.size()==3){
+            return 6;
+        }
+        else{
+            return 0;
+        }
     }
-     if(command[0]=="MPRINT"){
-        
+    if(command[0]=="MPRINT"){
+        if(command.size()==1){
+            return 7;
+        }
+        else{
+            return 0;
+        }
     }
      if(command[0]=="APRINT"){
-        
+        if(command.size()==1){
+            return 8;
+        }
+        else{
+            return 0;
+        }
     }
      if(command[0]=="SPATH"){
-        
+        if(command.size()==3){
+            return 9;
+        }
+        else{
+            return 0;
+        }
     }
-
+    else{
+        return 0;
+    }
 }
 
 int main(){
-    vector<vector<vector<int>>> graphs;
-    vector<string> command=inputter(){
+    vector<Node*> graph;
+    int noOfvertices;
+    vector<string> command=inputter(); 
+    bool directed;
+    while(command[0]!="EXIT"){
+        if (inputDetect(command,graph)==0){
+            cout<<"UNSUPPORTED COMMAND"<<endl;
+        }
+        else if (inputDetect(command, graph)==1){ //DINIT
+            directed=true;
+            int noOfvertices=stoi(command[1]);
+        }
+        else if (inputDetect(command, graph)==2){ //UINIT
+            directed=false;
+            int noOfvertices=stoi(command[1]);
+        }
+        else if (inputDetect(command, graph)==3){ //VADD
+            Node* newNode;
+            newNode->data=command[1];
+            graph.push_back(newNode);
+        }
+        else if (inputDetect(command, graph)==4){ //EADD //1st vertex to 2nd vertex IF directed
+            if(directed){
+                Node* toNode;
+                Node* fromNode;
+                for(int i=0; i < graph.size();i++){
+                    if(graph[i]->data==command[1]){
+                        fromNode=graph[i]; // find first vertex
+                    }
+                }
+                for(int i=0; i < graph.size(); i++){
+                    if(graph[i]->data==command[2]){ // find second vertex
+                        toNode=graph[i];
+                    }   
+                }
+                toNode->Tweight=stoi(command[3]);
+                fromNode->to.push_back(toNode); 
+                toNode->from.push_back(fromNode); //this and the command above essentially creates an edge
+            }
+            else{
+                Node* toNode;
+                Node* fromNode;
+                for(int i=0; i < graph.size(); i++){
+                    if(graph[i]->data==command[2]){ // find 2nd vertex
+                        toNode=graph[i];
+                    }  
+                    
+                }
+                for(int i=0; i < graph.size();i++){
+                    if(graph[i]->data==command[1]){
+                        fromNode=graph[i];
+                    }
+                }
 
-    }
+                toNode->Tweight=stoi(command[3]);
+                fromNode->Tweight=stoi(command[3]);
+                fromNode->to.push_back(toNode);
+                toNode->from.push_back(fromNode);
+                
+                // an undirected graph is relaly just a fully bidirectional directed graph;
+            }
+        }
+        else if (inputDetect(command, graph)==5){ //VDEL
+            for(int i=0; i<graph.size(); i++){
+                // iterate through toNodes and fromNodes of everything, find which ones contain the vertex to be deleted, delete them
+                // then, only then can you delete the vertex
+            }
+        }
+        else if (inputDetect(command, graph)==6){ //EDEL
+            //iterate through toNodes and fromNodes like before, find which ones contain the
+        }
+        else if (inputDetect(command, graph)==7){ //MPRINT
 
+        }
+        else if (inputDetect(command, graph)==8){ //APRINT
+
+        }
+        else if (inputDetect(command, graph)==9){ //SPATH
+
+        }
+        command=inputter();
     }
 }
