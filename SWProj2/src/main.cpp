c8668334ff74d2fff3aaeac64341b99dd23080b8
sufeeeringGavinM <gavin.christian.montevirgen@eee.upd.edu.ph>
2024-05-27 23:46:21 +0800

    updated edgecomparer to be more like the one in lectureslides

--- SWProj2/src/main.cpp
+++ SWProj2/src/main.cpp
@@ -8,8 +8,6 @@
 
 using namespace std;
 
-typedef pair<int,int> PII;
-
 struct Edge{
     int u;
     int v;
@@ -50,48 +48,49 @@ class UnionFind{
             }
             return false;
         }
-
 };
 
 bool edgeComparer(Edge a, Edge b){
-    return a.weight > b.weight;
+    if(a.weight != b.weight){
+        return a.weight > b.weight;
+    }
+    else if(a.u != b.u){
+        return a.u > b.u;
+    }
+    return a.v > b.v;
 }
 
-vector<Edge> maxTree(int n, vector<Edge> edges){
-    sort(edges.begin(),edges.end(),edgeComparer);
-    UnionFind unionf(n);
+vector<Edge> maxTree(int nwa, vector<Edge> air2air){
+    sort(air2air.begin(),air2air.end(),edgeComparer);
+    UnionFind unionf(nwa);
     vector<Edge> Mst;
-    for(int i=0; i<edges.size(); i++){
-        if(unionf.unyon(edges[i].u, edges[i].v)){
-            Mst.push_back(edges[i]);
+    for(int i=0; i<air2air.size(); i++){
+        if(unionf.unyon(air2air[i].u, air2air[i].v)){
+            Mst.push_back(air2air[i]);
         }
     }
     return Mst;
 }
 
-vector<vector<Edge>> adjCreator(int n, vector<Edge> edges){
-    vector<vector<Edge>> adj(n);
-    for(Edge edge : edges) {
-        adj[edge.u].emplace_back(edge.u, edge.v, edge.weight);
-        adj[edge.v].emplace_back(edge.v, edge.u, edge.weight); // Since the graph is undirected
-    }
-    return adj;
-}
-
-vector<vector<int>> closestAirports(int n, int k, vector<Edge> Adj){ 
+vector<vector<int>> closestAirports(int ncities, int nwa, vector<Edge> country){ //n is n of cities, k is n of 
     // this is really just djkistra but lets initialize distance of those with airports as 0
-    vector<vector<Edge>> adj = adjCreator(n,Adj); //create adjlist
-    
-    vector<int> d_s(n, INT_MAX); //preinitialize to intmax
-    vector<int> pre(n, -1);
+    vector<vector<Edge>> adj(ncities);
+    for(int i=0; i<country.size(); i++) {
+        adj[country[i].u].emplace_back(country[i].u, country[i].v, country[i].weight);
+        adj[country[i].v].emplace_back(country[i].v, country[i].u, country[i].weight); // Since the graph is undirected
+    } 
+
+    vector<int> d_s(ncities, INT_MAX); //preinitialize to intmax, essentially infinity
+    vector<int> pre(ncities, -1);
     
     int u,w;
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq; 
 
-    for(int i=0; i<k;i++){
+    for(int i=0; i<nwa;i++){
         d_s[i]=0;
         pq.push({i,0});
     }
+
     while(!pq.empty()){
         u=pq.top().first;
         w=pq.top().second;
@@ -119,9 +118,7 @@ vector<int> pathFinder(int start, vector<int> pre){ //thisll make life easier fo
     for(int i=start; i!=-1; i=pre[i]){
         path.push_back(i);
     }
-   
     return path;
-
 }
 
 int main(){
@@ -134,36 +131,38 @@ int main(){
     vector<Edge> air2air; // cities with airport-to-airport will be moved here
     vector<Edge> ProfitableFlights;
     vector<vector<int>> closestAirportss;
+    vector<int> path;
 
     for(int a=0; a<testcases; a++){
         cin >> ncities >> nwa >> nedges;
         
         //reinit stuff
-        maxProfit=0;
+        maxProfit=0; 
         country.clear();
         air2air.clear();
         ProfitableFlights.clear();
         closestAirportss.clear();
-        string line;
-        vector<string> lines;
-        string findme="";
         
         // generate city nodes in graph also filename is based on N K and M
         string filename = "graph-diagram-";
+        // filename+=to_string(a+1);
+        // filename+="-";
+        // filename+=to_string(ncities);
+        // filename+="-";
+        // filename+=to_string(nwa);
+        // filename+="-";
+        // filename+=to_string(nedges);
+        // filename+=".dot";
+        filename+="testcase-";
         filename+=to_string(a+1);
-        filename+="-";
-        filename+=to_string(ncities);
-        filename+="-";
-        filename+=to_string(nwa);
-        filename+="-";
-        filename+=to_string(nedges);
         filename+=".dot";
         ofstream dotfile(filename);
         
-        dotfile << "strict graph G{";
-        dotfile << "    fontname=\"Helvetica,Arial,sans-serif\"" <<endl;
-        dotfile << "    node [fontname=\"Helvetica,Arial,sans-serif\",colorscheme=greens9]" <<endl;
-        dotfile << "    edge [fontname=\"Helvetica,Arial,sans-serif\"]" << endl;
+        dotfile << "strict graph G{"; //strict so i dont have to find/replace every edge that turns out to be shortest distance to airport or max profit that i originally initialized as a simple gray dotted/solid line
+                                      // i tried the "find/replace every line containing this edge" and it was too much of a headache!
+        dotfile << "    fontname=\"Papyrus\"" <<endl;
+        dotfile << "    node [fontname=\"Papyrus\",colorscheme=greens9]" <<endl;
+        dotfile << "    edge [fontname=\"Comic Sans MS\"]" << endl;
         for(int i=0; i<nwa; i++){
             dotfile << i <<" [label=\"City-" << i << "\", style=filled, color=8,fontcolor=white]" << endl;
         }
@@ -201,12 +200,11 @@ int main(){
         for (int j=0; j<ProfitableFlights.size(); j++) {
             cout << ProfitableFlights[j].u << " " << ProfitableFlights[j].v << " " << ProfitableFlights[j].weight << endl;
             maxProfit+=ProfitableFlights[j].weight;
-
             dotfile << ProfitableFlights[j].u << " -- " << ProfitableFlights[j].v << " [label=" << ProfitableFlights[j].weight << ", style=dashed, color=blue]" << endl;
         }
         
         // B. distance of each city to nearest airport
-        // we use djkistra
+        // we use djkistra, but each city with an airport  has a d_s of zero
         
         closestAirportss=closestAirports(ncities,nwa,country);
         
@@ -214,11 +212,14 @@ int main(){
         for (int i = nwa; i < ncities; i++) {
             //cout << i << ": " << closestAirportss[0][i] << "\n";
             cout << i << ": " << closestAirportss[0][i] << "\n";
-            vector<int> path=pathFinder(i,closestAirportss[1]);
+            path=pathFinder(i,closestAirportss[1]);
             
-            for(int j=0; j<path.size()-1;j++){ // find what edges and what weights to change for the graph.
+            for(int j=0; j<path.size()-1;j++){ // find what edges and what weights to add for the graph based on the path
                 for(int z=0; z<country.size(); z++){
                     if((country[z].u == path[j] && country[z].v == path[j+1])||((country[z].v == path[j] && country[z].u == path[j+1]))){
+                        dotfile << path[j] << " -- " << path[j+1] << " [label=" << country[z].weight << ", style=line, color=blue]" << endl;
+                    }
+                    else if((country[z].u == path[j+1] && country[z].v == path[j])||((country[z].v == path[j+1] && country[z].u == path[j]))){
                         dotfile << path[j] << " -- " << path[j+1] << " [label=" << country[z].weight << ", style=line, color=blue]" << endl;
                     }
                 }
